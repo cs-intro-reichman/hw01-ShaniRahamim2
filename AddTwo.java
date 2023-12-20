f6821b715478677037adb4be49c28417cfcd6625
ShaniRahamim2 <shani.rahamim@post.runi.ac.il>
2023-12-20 14:49:00 +0200

    Update AddTwo.java

--- AddTwo.java
+++ AddTwo.java
@@ -1,9 +1,6 @@
-/*
- * Adds two given integers and prints the result in a fancy way.
- */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+
         int a = Integer.parseInt(args[0]);
         int b = Integer.parseInt(args[1]);
 
